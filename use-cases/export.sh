97389df1eb787611c2a73f37fa7f663a47b64d86
mandolyte <cecil.new@gmail.com>
2024-10-19 15:53:13 +0100

    Added script for regular style channels

---
+++ use-cases/export.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+###
+### NOTE: this is for a forum style channel
+### where each post is a thread.
+###
+
+BINDIR=$HOME/Downloads/zz
+CLI=DiscordChatExporter.Cli
+EXEC=$BINDIR/$CLI
+TOKEN=`cat $HOME/.discord`
+FOLDER=./data
+PWD=`pwd`
+CHANNEL=`basename $PWD`
+CHANNELID=1124403655819415592
+
+echo Working channel $CHANNEL with ID $CHANNELID
+
+rm -rf $FOLDER
+mkdir $FOLDER
+
+echo Start `date`
+
+$EXEC export \
+    --channel $CHANNELID \
+    --token $TOKEN \
+    --output $FOLDER/$CHANNEL.txt \
+    --format PlainText \
+    > export.log 2>&1 
+
+echo Done `date`
